56f47ac62f3043b9c12c269303f5d7fe3fc2a66f
dorianascimento <rafaelnd2001@gmail.com>
2023-04-01 21:45:01 -0300

    feat: add express server setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  return res.json({ message: 'hello world' });
+});
+
+app.use((req: Request, res: Response) => {
+  return res.status(404).json({ error: 'endpoint not found' });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('server is running on port 3333');
+});
