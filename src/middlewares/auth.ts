ba5bc30c952fc8aed9dcea5c4d677e0f449c4629
dorianascimento <rafaelnd2001@gmail.com>
2023-04-13 18:15:05 -0300

    feat: add middleware to auth user

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import JWT from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export interface AuthRequest extends Request {
+  userId: string;
+}
+
+export function Auth(req: AuthRequest, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'not authorized' });
+  }
+
+  const [type, token] = authHeader.split(' ');
+
+  if (type === 'Bearer') {
+    try {
+      const decodedToken = JWT.verify(token, process.env.JWT_SECRET_KEY as string) as { id: string };
+      req.userId = decodedToken.id;
+      next();
+    } catch (error) {
+      return res.status(401).json({ error: 'invalid token' });
+    }
+  } else {
+    return res.status(401).json({ error: 'only bearer type' });
+  }
+}
